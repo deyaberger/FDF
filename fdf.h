2a98f56a8053c52ff279247c564311b74beb346a
Nicolas COURSOL <ncoursol@e1r11p4.42.fr>
2019-07-03 11:58:01 +0200

    ajout check fct

--- fdf.h
+++ fdf.h
@@ -6,24 +6,25 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 18:57:35 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/07/02 14:47:33 by dberger          ###   ########.fr       */
+/*   Updated: 2019/07/02 17:26:20 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include "./sources/GNL/get_next_line.h"
-#include "./sources/ft_printf/ft_printf.h"
-#include "./sources/minilibx_macos/mlx.h"
+#include "includes/GNL/get_next_line.h"
+#include "includes/ft_printf/ft_printf.h"
+#include "includes/minilibx/mlx.h"
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <stdio.h>
 
 typedef struct	s_struct
 {
 	void	*mlx;
 	void	*win;
-	void	*img;
 	int		ax;
 	int		ay;
 	int		bx;
@@ -31,6 +32,7 @@ typedef struct	s_struct
 	int		col;
 }				t_struct;
 
+int		ft_check_fdf(char *buf);
 void	tracer_segment(t_struct *t, int col);
 
 #endif
--- main.c
+++ main.c
@@ -6,58 +6,39 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 15:47:18 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/07/02 14:48:08 by dberger          ###   ########.fr       */
+/*   Updated: 2019/07/03 11:29:45 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int		mouse_move(int x, int y, t_struct *t)
-{
-	t->bx = x;
-	t->by = y;
-	t->col += 100000;
-	tracer_segment(t, t->col);
-	return (0);
-}
-
-/*int		mouse_press(int button, int x, int y, t_struct *t)
-{
-	t->bx = x;
-	t->by = y;
-	if (button == 1)
-	{
-		t->col += 100000;
-		tracer_segment(t, t->col);
-	}
-	return (0);
-}
-*/
 int		deal_key(int key, t_struct *t)
 {
-
-	ft_printf("key : [%d]\n", key);
-	if (key == 0)
-		ft_printf("merci nico\n");
 	if (key == 53)
 		exit(0);
 	return (0);
 }
 
-int		main()
+int		main(int argc, char **argv)
 {
 	t_struct	t;
+	int			fd;
+	char		buf[2084];
+	int			k;
+	int i;
 
+	i = 0;
+	fd = open(argv[1], O_RDONLY);
+	k = read(fd, buf, 2084);
+	buf[k] = '\0';
+	if (argc != 2 || !ft_check_fdf(buf))
+	{
+	ft_printf(" ERREUR! %d\n", argc);
+		return (0);
+	}
 	t.mlx = mlx_init();
-	t.win = mlx_new_window(t.mlx, 1000, 700, "FDF - Nico");
-	t.ax = 1;
-	t.ay = 1;
-	t.bx = 100;
-	t.by = 100;
-	t.col = 0x00FF00FF;
+	t.win = mlx_new_window(t.mlx, 2000, 1000, "FDF - Nico");
 	mlx_hook(t.win, 2, 0, deal_key, &t);
-	mlx_hook(t.win, 6, 0, mouse_move, &t);
-//	mlx_hook(t.win, 4, 0, mouse_press, &t);
 	mlx_loop(t.mlx);
 	return (0);
 }
