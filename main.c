60c076a8c9c0b36d4cd5dcaa161aa9290a3a8f3d
Nicolas COURSOL <ncoursol@e1r13p5.42.fr>
2019-07-04 14:53:53 +0200

    ajout image main

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 15:47:18 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/07/03 11:29:45 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/07/04 14:53:34 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	fill_pixel(char *my_img, int x, int y, int color)
+{
+	int		i;
+
+	i = (((HEIGHT - (HEIGHT - y)) * WIDTH) - (WIDTH - x)) * 4;
+	my_img[i + 0] = ((color & 0xff000000) >> 24);
+	my_img[i + 1] = ((color & 0x00ff0000) >> 16);
+	my_img[i + 2] = ((color & 0x0000ff00) >> 8);
+	my_img[i + 3] = ((color & 0x000000ff) >> 0);
+}
 
 int		deal_key(int key, t_struct *t)
 {
@@ -25,9 +36,12 @@ int		main(int argc, char **argv)
 	int			fd;
 	char		buf[2084];
 	int			k;
-	int i;
+	void		*pt_img;
+	char		*my_img;
+	int			bpp;
+	int			s_l;
+	int			endian;
 
-	i = 0;
 	fd = open(argv[1], O_RDONLY);
 	k = read(fd, buf, 2084);
 	buf[k] = '\0';
@@ -37,7 +51,11 @@ int		main(int argc, char **argv)
 		return (0);
 	}
 	t.mlx = mlx_init();
-	t.win = mlx_new_window(t.mlx, 2000, 1000, "FDF - Nico");
+	t.win = mlx_new_window(t.mlx, 800, 600, "FDF - Nico");
+	pt_img = mlx_new_image(t.mlx, 800, 600);
+	my_img = mlx_get_data_addr(pt_img, &(bpp), &(s_l), &(endian));
+
+	mlx_put_image_to_window(t.mlx, t.win, pt_img, 0, 0);
 	mlx_hook(t.win, 2, 0, deal_key, &t);
 	mlx_loop(t.mlx);
 	return (0);
