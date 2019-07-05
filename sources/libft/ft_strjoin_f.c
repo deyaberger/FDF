3cf59a81c335a004c270373bde5bdf1d5a619b69
Nicolas COURSOL <ncoursol@e1r12p4.42.fr>
2019-07-05 11:53:15 +0200

    modif lib

--- sources/libft/ft_strjoin_f.c
+++ sources/libft/ft_strjoin_f.c
@@ -3,28 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin_f.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/15 13:52:42 by dberger           #+#    #+#             */
-/*   Updated: 2019/04/26 14:01:40 by dberger          ###   ########.fr       */
+/*   Created: 2019/04/13 23:29:32 by ncoursol          #+#    #+#             */
+/*   Updated: 2019/07/05 11:51:27 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin_f(char *s1, char *s2, int i)
+void	ft_free(const char *s1, const char *s2, int *a)
 {
-	char	*new;
+	if (*a == 1 || *a == 3)
+		free((char*)s1);
+	if (*a == 2 || *a == 3)
+		free((char*)s2);
+}
 
-	if (!s1 || !s2)
+char	*ft_strjoin_f(char const *s1, char const *s2, int a)
+{
+	char	*str;
+	int		len;
+
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	if (!(new = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(*str) * (len + 1))))
 		return (NULL);
-	ft_strcpy(new, s1);
-	ft_strcat(new, s2);
-	if (i == 1 || i == 3)
-		free((char *)s1);
-	if (i == 2 || i == 3)
-		free((char *)s2);
-	return (new);
+	str[len] = '\0';
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	ft_free(s1, s2, &a);
+	return (str);
 }
