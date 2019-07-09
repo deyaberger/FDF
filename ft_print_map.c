48cb3a3105e00c3b4a2f763dda143f8416527bd9
Nicolas COURSOL <ncoursol@e1r10p2.42.fr>
2019-07-09 14:45:53 +0200

    ajout etape 2 fonctionel

--- ft_print_map.c
+++ ft_print_map.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 16:54:53 by dberger           #+#    #+#             */
-/*   Updated: 2019/07/09 13:47:32 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/07/09 14:43:16 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_print_map(t_struct *t, char *my_img, char **tab)
 	int		sv;
 
 	i = 0;
-	t->col--;
+	//t->col--;
 	while (tab[i])
 	{
 		t->bx = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
@@ -30,7 +30,7 @@ void	ft_print_map(t_struct *t, char *my_img, char **tab)
 		t->by = t->by * cos((30 * (M_PI / 180)));
 
 		sv = i;
-		if (i + 1 <= (t->col * t->line) - 1)
+		if ((i + 1) % (t->col) != 0)
 		{
 			i++;
 			t->ax = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
@@ -41,9 +41,11 @@ void	ft_print_map(t_struct *t, char *my_img, char **tab)
 			t->ay = t->ay * cos((30 * (M_PI / 180)));
 			ft_trace_line(t, my_img, 0xFF000000);
 		}
-		if (((i - 1) + t->col + 1) <= (t->col * t->line) - 1)
+		else
+			i++;
+		if (((i - 1) + t->col) <= (t->col * t->line) - 1)
 		{
-			i = ((i - 1) + t->col + 1);
+			i = ((i - 1) + t->col);
 			t->ax = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
 			t->ay = (((i / t->col) + 1) * SPACE) + STARTY;
 			save = t->ax;
