ba9df14fa67f4bbef42f8c87976d86863ec90987
Nicolas COURSOL <ncoursol@e1r10p2.42.fr>
2019-07-09 13:48:08 +0200

    draw 2 etapes rotation

--- ft_print_map.c
+++ ft_print_map.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 16:54:53 by dberger           #+#    #+#             */
-/*   Updated: 2019/07/09 11:37:37 by dberger          ###   ########.fr       */
+/*   Updated: 2019/07/09 13:47:32 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,43 @@
 void	ft_print_map(t_struct *t, char *my_img, char **tab)
 {
 	int		i;
-	int		c;
-	int		l;
-	int		s;
-	int		x;
-	int		y;
-	int		z;
-	int		coor[t->col * t->line][3];
+	int		save;
+	int		sv;
 
 	i = 0;
-	l = 0;
-	c = 0;
-	s = SPACE;
-
-	// segments de departs
-	t->ax = STARTX;
-	t->bx = t->ax;
-	t->ay = 0;
-	t->by = 600;
-	ft_trace_line(t, my_img, COLOR);
-	t->ax = 0;
-	t->bx = 800;
-	t->ay = STARTY;
-	t->by = t->ay;
-	ft_trace_line(t, my_img, COLOR);
-	// fin segments de depart
-	// line = i - (col * (i / col));
-	// col = i / col;
-
-	t->ax = STARTX;
-	t->ay = STARTY;	
-	while (tab[i] && l < t->line)
+	t->col--;
+	while (tab[i])
 	{
-		x = t->ax + s;
-		y = t->ay;
-		z = ft_atoi(tab[i]);
-		coor[i][0] = z;
-		coor[i][1] = (x - y) * cos(30 * (M_PI / 180));
-		coor[i][2] = (x + y) * sin(30 * (M_PI / 180));
-		printf("coor[%d][z]=%d, coor[%d][x]=%d, coor[%d][y]=%d\n", i, coor[i][0], i, coor[i][1], i, coor[i][2]);
-		i++;
-		if (c == t->col - 1)
+		t->bx = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
+		t->by = (((i / t->col) + 1) * SPACE) + STARTY;
+		save = t->bx;
+		t->bx = t->bx * cos(30 * (M_PI / 180)) - t->by * sin(30 * (M_PI / 180));
+		t->by = save * sin((30 * (M_PI / 180))) + t->by * cos((30 * (M_PI / 180)));
+		t->by = t->by * cos((30 * (M_PI / 180)));
+
+		sv = i;
+		if (i + 1 <= (t->col * t->line) - 1)
 		{
-			coor[i][1] = coor[i - t->col][1];
-			coor[i][2] = coor[i - t->col][2];
-			s = SPACE;
-			y += s;
-			x= t->ax + s;
-			c = 0;
-			printf("\n");
-			printf("i=%d. i--t->col=%d\n", i, i-t->col);
-			l++;
+			i++;
+			t->ax = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
+			t->ay = (((i / t->col) + 1) * SPACE) + STARTY;
+			save = t->ax;
+			t->ax = t->ax * cos((30 * (M_PI / 180))) - t->ay * sin((30 * (M_PI / 180)));
+			t->ay = save * sin((30 * (M_PI / 180))) + t->ay * cos((30 * (M_PI / 180)));
+			t->ay = t->ay * cos((30 * (M_PI / 180)));
+			ft_trace_line(t, my_img, 0xFF000000);
 		}
-		else
-			c++;
-		s += 30;
+		if (((i - 1) + t->col + 1) <= (t->col * t->line) - 1)
+		{
+			i = ((i - 1) + t->col + 1);
+			t->ax = (((i - (t->col * (i / t->col))) + 1) * SPACE) + STARTX;
+			t->ay = (((i / t->col) + 1) * SPACE) + STARTY;
+			save = t->ax;
+			t->ax = t->ax * cos((30 * (M_PI / 180))) - t->ay * sin((30 * (M_PI / 180)));
+			t->ay = save * sin((30 * (M_PI / 180))) + t->ay * cos((30 * (M_PI / 180)));
+			t->ay = t->ay * cos((30 * (M_PI / 180)));
+			ft_trace_line(t, my_img, 0x00FF0000);
+		}
+		i = sv + 1;
 	}
 }
